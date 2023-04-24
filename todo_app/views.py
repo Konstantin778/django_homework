1fbda8300ac792580ed3af9b55ced252836d5d39
Konstantin <oreshkin007@mail.ru>
2023-04-24 22:35:03 +0300

    24.04 + 1

--- todo_app/views.py
+++ todo_app/views.py
@@ -5,21 +5,25 @@ from django.shortcuts import render
 from todo_app.models import TodoTask, TodoStatus, TodoList
 
 
+def __create_task(request):
+    todo = TodoTask()
+    todo.title = request.POST.get('title')
+    todo.status = TodoStatus.objects.get(pk=TodoStatus.C_NOT_COMPLETED)
+    todo.create_at = datetime.now()
+    todo.text = ''
+    todo.todo_list = TodoList.objects.get(pk=1)
+    todo.save()
+
+
 def index(request):
     if request.method == 'POST':
-        type = request.POST.get('type')
-        if type == 'create':
-            todo = TodoTask()
-            todo.title = request.POST.get('title')
-            todo.status = TodoStatus.objects.get(pk=TodoStatus.C_NOT_COMPLETED)
-            todo.create_at = datetime.now()
-            todo.text = ''
-            todo.todo_list = TodoList.objects.get(pk=1)
-            todo.save()
-        if type == 'destroy':
+        action_type = request.POST.get('type')
+        if action_type == 'create':
+            __create_task(request)
+
+        if action_type == 'destroy':
             task_id = request.POST.get('task_id')
-            todo = TodoTask.objects.get(pk=task_id)
-            todo.delete()
+            TodoTask.objects.get(pk=task_id).delete()
 
     return render(request, 'todo_main.html', {
         'tasks_array': TodoTask.objects.all().order_by('-id')
